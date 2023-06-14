4cc0fb8ba6f0a13ed14b58963544ae270147b167
Sam Flower <66141971+flowerco@users.noreply.github.com>
2023-06-14 07:30:10 +0100

    Updated: fixed small text in auth form

--- src/components/auth/Card.jsx
+++ src/components/auth/Card.jsx
@@ -1,7 +1,7 @@
 const Card = ({ className, children }) => {
   return (
     <div
-      className={`rounded-3xl px-10 py-4 drop-shadow-xl
+      className={`rounded-3xl px-8 sm:px-10 py-4 drop-shadow-xl
         bg-white ${className}`}
     >
       {children}
